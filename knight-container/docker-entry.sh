b73a6a368ad946af4909dd0b5ad08bfcec3bf843
Dirk Uys <dirkcuys@gmail.com>
2015-09-11 14:05:00 -0600

    Add support for celery for knight container

--- knight-container/docker-entry.sh
+++ knight-container/docker-entry.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-
-#!/bin/bash
 set -e
 
 if [ "$1" = 'supervisord' ]; then
+    chown -R celery:celery /var/lib/celery
     /var/django-venv/bin/python /var/app/manage.py syncdb --noinput
     /var/django-venv/bin/python /var/app/manage.py collectstatic --noinput
     exec "$@"
