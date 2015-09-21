d8ad18fc74a3271ab14c772b846cf363476d0605
Dirk Uys <dirkcuys@gmail.com>
2015-09-21 16:07:36 -0600

    Switch from using django syncdb to using migrate

--- knight-container/docker-entry.sh
+++ knight-container/docker-entry.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ "$1" = 'supervisord' ]; then
     chown -R celery:celery /var/lib/celery
-    /var/django-venv/bin/python /var/app/manage.py syncdb --noinput
+    /var/django-venv/bin/python /var/app/manage.py migrate --noinput
     /var/django-venv/bin/python /var/app/manage.py collectstatic --noinput
     exec "$@"
 fi
--- subscribe-container/docker-entry.sh
+++ subscribe-container/docker-entry.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-
-#!/bin/bash
 set -e
 
 if [ "$1" = 'supervisord' ]; then
     chown -R celery:celery /var/lib/celery
-    /var/django-venv/bin/python /var/app/manage.py syncdb --noinput
+    /var/django-venv/bin/python /var/app/manage.py migrate --noinput
     /var/django-venv/bin/python /var/app/manage.py collectstatic --noinput
     exec "$@"
 fi
